631864ccd9b10a1f5b920de87a42c574b200f6c0
Yin Hong Tao <z123123123111>
2023-12-29 16:20:40 +0900

    Update configuration and styles

---
+++ src/app/components/logo/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import CarLogoImg from "../../../assets/images/car-logo.png";
+import tw from "twin.macro";
+import styled from "styled-components";
+
+const LogoContainer = styled.div`
+    ${tw`
+        flex
+        items-center
+    `};
+`
+
+const LogoText = styled.div`
+    ${tw`
+        text-xl
+        md:text-2xl
+        font-bold
+        text-black
+        m-1
+    `};
+`
+
+const Image = styled.div`
+    width: auto;
+    ${tw`
+        h-6 md:h-9
+    `};
+    img {
+        width: auto;
+        height: 100%;
+    }
+`
+
+export function Logo() {
+  return (
+    <LogoContainer>
+      <Image>
+        <img src={CarLogoImg} alt="car logo" />
+      </Image>
+      <LogoText>YourCar.</LogoText>
+    </LogoContainer>
+  );
+}
